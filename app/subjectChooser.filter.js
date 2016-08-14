e310557d6200c1c38a023cb773ec1277effedbcd
Nils Beyer <Nils Beyer>
2016-08-14 22:04:32 +0200

    Changed Select Method

--- app/subjectChooser.filter.js
+++ app/subjectChooser.filter.js
@@ -6,9 +6,9 @@ angular.module('stundenplanApp')
           search = search ? search : "";
           angular.forEach(items, function (value, key) {
               angular.forEach(value, function (value2, key2) {
-                  if (value2.toLowerCase().startsWith(search.toLowerCase()) && key2 == "name" && value2 != scope.currentSubject[scope.attrs.hour].name) {
+                  if (value2.toLowerCase().startsWith(search.toLowerCase()) && key2 == "name" && value2 != scope.currentDay[scope.attrs.hour].name) {
                       result.push(value2);
-                  } else if (value2.toLowerCase().startsWith(search.toLowerCase()) && key2 == "name" && value2 == scope.currentSubject[scope.attrs.hour].name) {
+                  } else if (value2.toLowerCase().startsWith(search.toLowerCase()) && key2 == "name" && value2 == scope.currentDay[scope.attrs.hour].name) {
 
                       result.unshift(value2)
                   }
--- app/subjectPicker.directive.js
+++ app/subjectPicker.directive.js
@@ -1,45 +1,31 @@
 angular.module('stundenplanApp')
-  .directive('subjectPicker', function(subjectDataService, $compile)  {
+  .directive('subjectPicker', function(subjectDataService, $compile, avaibleSubjects)  {
     return {
       restrict: 'E',
       replace: 'false',
-      template: `<p onclick="$(this).find('span').popover('toggle')" style="color: {{(subjectDataService.getHour(attrs.week, attrs.day, attrs.hour).color == 'white') ? 'black' : subjectDataService.getHour(attrs.week, attrs.day, attrs.hour).color}}">{{subjectDataService.getHour(attrs.week, attrs.day, attrs.hour).name}} <span class='glyphicon glyphicon-edit' aria-hidden='true' tabindex='0' data-trigger='manual'></span></p>`,
+      template: `
+                <select>
+                  <option ng-repeat="(k,v) in avaibleSubjects | subjectFilter: '':this"
+                          ng-selected="v == currentDay[attrs['hour']].name"
+                          >
+                          {{v}}
+                  </option>
+                </select>
+                `,
       scope: {},
       link: function(scope, elem, attrs){
-        scope.subjectDataService = subjectDataService
+        scope.data = subjectDataService
         scope.attrs = attrs;
         scope.color = subjectDataService
+        scope.avaibleSubjects = avaibleSubjects;
+        scope.currentDay = subjectDataService.data[attrs['week']][attrs['day']];
 
-
-        let placement = "right";
-        if (attrs.day == "Mittwoch" || attrs.day == "Donnerstag" || attrs.day == "Freitag")  {
-          placement = "left";
-        }
-
-        $(elem).find('span').popover({
-          'placement': placement,
-          html: true,
-          content: $compile("<subject-Chooser day='{{attrs.day}}' week='{{attrs.week}}' hour='{{attrs.hour}}'></subject-Chooser>")(scope),
-          title: "<span style='color: black'>Fach auswählen</span>"
-        }).on('click', (e) => {
-            e.preventDefault();
-            return true;
-        }).on('shown.bs.popover', () => {
-          $(elem).find(".popover-content input").focus();
-        }).on('show.bs.popover', () => {
-          setTimeout(() => {
-            let selectedSpan = $(elem).find('span');
-            $('span').each(function(index) {
-                if ($(this)[0] != selectedSpan[0]) {
-
-                  $(this).popover('hide');
-                }
-            })
-          }, 0)
+        $(elem).change(function() {
+          let newSubject = $(elem).find('option:selected').text();
+          scope.$apply(() => {
+            scope.currentDay[attrs['hour']] = avaibleSubjects[newSubject];
+          })
         })
-
-
-
       }
     }
   })
