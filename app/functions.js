40fbca2fdb06543b659488a17b6ae033597cb7b1
Nils Beyer <Nils Beyer>
2016-08-15 15:58:12 +0200

    Adding colors to new Select ChooseR

---
+++ app/functions.js
@@ -0,0 +1,14 @@
+// A Simple Funktion Libary, nothing to do with Angular or even jQuery :-)
+
+/*
+    Transferes a HEX Color Code (like #00FFFF) to an RGB object.
+      Hex: HEX Color Code
+*/
+let hexToRgb = (hex) => {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : {r: 0, g: 0, b: 0};
+}
